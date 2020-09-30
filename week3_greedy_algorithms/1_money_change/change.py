b09e853036ac74572afbc4114f8ef8d4f6df46fa
xchen715 <wilsonhc715@gmail.com>
2020-09-29 22:21:53 -0400

    assignment: week 3 1st challenge complete

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -2,8 +2,12 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m
+    coins = 0
+    ris = m
+    for value in [10,5,1]:
+        coins += int(ris / value)
+        ris = ris % value
+    return coins
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
