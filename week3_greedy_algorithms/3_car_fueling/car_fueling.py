bfcbf6c1a04625e51115aac4c85abe07f761cb9f
xchen715 <wilsonhc715@gmail.com>
2020-09-30 19:04:59 -0400

    assignment: week3 3rd challenge complete

--- week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -3,28 +3,19 @@ import sys
 
 
 def compute_min_refills(distance, tank, stops):
-    if tank >= distance:
-        return -1
-    if len(stops) == 0 and distance > tank:
-        return -1
-
-    refill_stop = 0
+    full_stops = [0] + stops + [distance]
+    current_stop = 0
     refill_count = 0
-    while refill_stop
-    for stop in stops:
-        if tank + refill_stop > stop and distance - refill_stop > tank:
-            last_stop = stop
-        elif tank == stop - refill_stop:
-            refill_stop = stop
-            last_stop = stop
+
+    while current_stop <= len(stops):
+        last_refill = current_stop
+        while current_stop <= len(stops) and tank >= full_stops[current_stop + 1] - full_stops[last_refill]:
+            current_stop += 1
+        if current_stop == last_refill:
+            return -1
+        if current_stop <= len(stops):
             refill_count += 1
-        else:
-            refill_stop = last_stop
-            refill_count += 1
-    if refill_stop != 0:
-        return refill_count
-    else:
-        return -1
+    return refill_count
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())
--- week3_greedy_algorithms/3_car_fueling/test_car_fueling.py
+++ week3_greedy_algorithms/3_car_fueling/test_car_fueling.py
@@ -1,2 +1,23 @@
 import pytest
-from car_fueling import
+from car_fueling import compute_min_refills
+
+def test_compute_min_refills_1():
+    assert(compute_min_refills(950,400,[200,375,550,750]) == 2)
+
+def test_compute_min_refills_2():
+    assert(compute_min_refills(950,400,[200,375,550,750,950]) == 2)
+
+def test_compute_min_refills_3():
+    assert(compute_min_refills(10,3,[1,2,5,9]) == -1)
+
+def test_compute_min_refills_4():
+    assert(compute_min_refills(10,3,[1,2,5,8]) == 3)
+
+def test_compute_min_refills_5():
+    assert(compute_min_refills(200,250,[100,150]) == 0)
+
+def test_compute_min_refills_6():
+    assert(compute_min_refills(200,150,[]) == -1)
+
+def test_compute_min_refills_7():
+    assert(compute_min_refills(700,100,[50, 150, 200, 275, 300, 400, 425, 525, 600, 700]) == 8)
