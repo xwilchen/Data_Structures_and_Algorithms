a473dfb1f29420c961f7f4cf6a00158894fc5b5b
xchen715 <wilsonhc715@gmail.com>
2020-09-29 23:38:58 -0400

    assignment: week 3 3rd challenge checkpoint

--- week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -3,8 +3,28 @@ import sys
 
 
 def compute_min_refills(distance, tank, stops):
-    # write your code here
-    return -1
+    if tank >= distance:
+        return -1
+    if len(stops) == 0 and distance > tank:
+        return -1
+
+    refill_stop = 0
+    refill_count = 0
+    while refill_stop
+    for stop in stops:
+        if tank + refill_stop > stop and distance - refill_stop > tank:
+            last_stop = stop
+        elif tank == stop - refill_stop:
+            refill_stop = stop
+            last_stop = stop
+            refill_count += 1
+        else:
+            refill_stop = last_stop
+            refill_count += 1
+    if refill_stop != 0:
+        return refill_count
+    else:
+        return -1
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())
---
+++ week3_greedy_algorithms/3_car_fueling/test_car_fueling.py
@@ -0,0 +1,2 @@
+import pytest
+from car_fueling import
