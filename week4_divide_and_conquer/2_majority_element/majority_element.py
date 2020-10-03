5d89c4ffb42e17961216973e89040ea452632b53
xchen715 <wilsonhc715@gmail.com>
2020-10-02 20:08:01 -0400

    assignment: week4 majority element challenge complete

--- week4_divide_and_conquer/2_majority_element/majority_element.py
+++ week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -1,18 +1,22 @@
 # Uses python3
 import sys
 
-def get_majority_element(a, left, right):
-    if left == right:
+def get_majority_element(a):
+    count = {}
+    for n in a:
+        if n not in count:
+            count[n] = 1
+        else:
+            count[n] += 1
+    if max(count.values()) * 2 > len(a):
+        return 1
+    else:
         return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    if get_majority_element(a) != -1:
         print(1)
     else:
         print(0)
