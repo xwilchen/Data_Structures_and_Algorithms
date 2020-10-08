6c60b228942408a87d71cd2e4bc6b2f87e90ce29
xchen715 <wilsonhc715@gmail.com>
2020-10-08 19:32:07 -0400

    week3 summands

--- week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -2,9 +2,18 @@
 import sys
 
 def optimal_summands(n):
+    if n <= 2:
+        return [n]
     summands = []
-    #write your code here
-    return summands
+    i = 1
+    left = n
+    while i <= left:
+        left -= i
+        if left <= i:
+            summands.append(left + i)
+            return summands
+        summands.append(i)
+        i += 1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
