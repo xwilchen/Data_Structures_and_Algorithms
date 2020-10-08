f86ca8685b451c59e3c35588c5ad30669866f1e4
xchen715 <wilsonhc715@gmail.com>
2020-10-08 19:23:19 -0400

    week3 max salary

--- week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -1,12 +1,22 @@
 #Uses python3
 
 import sys
+def greater_or_equal(d, max_d):
+    if int(str(d) + str(max_d)) >= int(str(max_d) + str(d)):
+        return True
+    else:
+        return False
 
 def largest_number(a):
     #write your code here
     res = ""
-    for x in a:
-        res += x
+    while a:
+        max_d = a[0]
+        for d in a:
+            if greater_or_equal(d, max_d):
+                max_d = d
+        res += str(max_d)
+        a.remove(max_d)
     return res
 
 if __name__ == '__main__':
