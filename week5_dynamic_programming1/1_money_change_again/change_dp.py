968b7505563db02ff0778baae917ef89922058be
xchen715 <wilsonhc715@gmail.com>
2020-10-17 15:03:31 -0400

    week5 change dp complete

--- week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -2,8 +2,19 @@
 import sys
 
 def get_change(m):
+    if m == 0:
+        return 0
+    denominators = [1, 3, 4]
+    min_coins_array = [0]
+    for i in range(1, m+1):
+        min_coins = []
+        for denom in denominators:
+            if i >= denom:
+                min_coins.append(min_coins_array[i - denom] + 1)
+        min_coins_array.append(min(min_coins))
+
     #write your code here
-    return m // 4
+    return min_coins_array[-1]
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
