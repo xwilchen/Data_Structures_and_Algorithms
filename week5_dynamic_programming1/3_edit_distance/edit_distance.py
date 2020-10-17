f1167e7f535152f965112033ca27d9c211f68a1e
xchen715 <wilsonhc715@gmail.com>
2020-10-17 17:21:27 -0400

    week5 edit distance

--- week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,7 +1,17 @@
+import numpy as np
 # Uses python3
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    dp_table = np.zeros((len(s) + 1, len(t) + 1))
+    dp_table[0] = range(len(t) + 1)
+    dp_table[:,0] = range(len(s) + 1)
+
+    for i in range(1, len(s) + 1):
+        for j in range(1, len(t) + 1):
+            if s[i - 1] == t[j - 1]:
+                dp_table[i, j] = dp_table[i - 1, j - 1]
+            else:
+               dp_table[i, j] = min(dp_table[i - 1, j - 1], dp_table[i - 1, j], dp_table[i, j - 1]) + 1
+    return int(dp_table[len(s), len(t)])
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
